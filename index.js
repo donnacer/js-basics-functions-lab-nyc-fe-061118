2a03e68d8a04fd812b7eb0562879de37610ece41
Donna Cerrio <denique86@yahoo.com>
2018-07-19 01:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+       
+ function  distanceFromHqInBlocks (street) {
+   if (street >=42) {
+   return street;
+ } else
+ return 42-street;
+ }
+       
+function distanceTravelledInFeet = (feet) {
+  if (feet >= 42) {
+    return (feet)
+  }
+
+
+ function  distanceFromHqInFeet
+       
+ function  calculatesFarePrice
